60b92d618f9f1522e8043073845e73cb30308636
yuta0306 <you-2001-3-6@ezweb.ne.jp>
2022-07-15 10:17:58 +0900

    add output option

--- src/cli.js
+++ src/cli.js
@@ -12,6 +12,7 @@ const parser = new ArgumentParser({
 
 parser.add_argument('-f', '--file', { help: 'input .md file' })
 parser.add_argument('-b', '--bibtex', { help: 'input .bib file' })
+parser.add_argument('-o', '--output', { default: false })
 parser.add_argument('--theme', { help: 'theme', default: 'gaia' })
 parser.add_argument('--generate', { default: false })
 parser.add_argument('--per-page', { default: 5 })
@@ -23,7 +24,10 @@ const result = await parse(args.bibtex)
 
 const out = await override(args.file, result, args.generate, args['per-page'])
 
-const promise = marpCli(['tmp.md', '--pdf', '--html=true', `--theme=${args.theme}`, `--watch=${args.watch}`])
+let output = args.output
+if (!output) output = args.file.replace(/\.md/, '.pdf')
+
+const promise = marpCli(['tmp.md', '--pdf', `--output=${output}`, '--html=true', `--theme=${args.theme}`, `--watch=${args.watch}`])
     .then((exitStatus) => {
         if (exitStatus > 0) {
             console.error(`Failure (Exit status: ${exitStatus})`)
